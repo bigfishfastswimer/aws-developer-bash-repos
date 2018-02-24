5d6cf47f6da60814600536b21459cda421c22f56
fisher snapper <fisher@gmail.com>
2018-02-24 23:05:05 +1100

    add Val_in_script.sh

---
+++ Val_in_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+date
+clear
+echo "This script will give us my account information"
+
+echo "================================================"
+
+echo ""
+STARTOFSCRIPT = `date`
+MYUSERNAME = "user"
+MYPASSWORD = "password123"
+echo "my login name is $MYUSERNAME"
+echo ""
+echo "Login user password is $MYPASSWORD"
+echo ""
+echo "I started this script at: $STARTOFSCRIPT"
+echo ""
+ENDOFSCRIPT = `date`
+echo "This script end at $ENDOFSCRIPT"
+echo ""
