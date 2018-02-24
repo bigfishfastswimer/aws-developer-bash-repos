765deb9b62f9f174e4281dbf4b3ca08c9f293038
Fisher <fisherhuang1986@gmail.com>
2018-02-24 11:37:57 +0000

    first push

---
+++ displayvar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+echo "This script will give us environment information"
+
+echo "================================================"
+
+echo ""
+echo "Home dir is $HOME"
+echo ""
+echo "Login user is $USER"
+echo ""
+echo "Bash history control is $HISTCONTROL"
+echo ""
+echo "Login terminal is $TERM"
+echo ""
